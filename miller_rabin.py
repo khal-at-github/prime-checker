bcf912165e347092e1c8c818e53e86c8714217bc
khal-at-github <123749356+khal-at-github@users.noreply.github.com>
2025-06-08 13:24:29 -0400

    Create miller_rabin.py

---
+++ miller_rabin.py
@@ -0,0 +1,23 @@
+#Miller-Rabin prime testAdd commentMore actions
+import random as r
+import numpy as np
+def prime_test(n):
+	if n == 3:
+		a=2
+	else:
+		a=r.randrange(2,2*round((np.log(n))**2))	
+	print(a)
+	if n == 2:
+		return True
+	y=n-1	
+	if (a**(y))%n==1:
+		while y%2==0:
+			y=y/2
+			x=(a**y)%n
+			if x == n-1:
+				return True
+			else: 
+				return False
+	else: 
+		return False
+Add comment
