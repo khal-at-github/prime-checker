9615c89fee7d93b7f8863f80e644b8996c7add3a
khal-at-github <123749356+khal-at-github@users.noreply.github.com>
2025-07-01 11:58:07 -0400

    Update miller-rabin.js

--- miller-rabin.js
+++ miller-rabin.js
@@ -1,26 +1,33 @@
 // Miller-Rabin prime test
-import Math
-function getRandomIntInclusive(min, max) {
+var getRandomIntInclusive = function(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-function prime_test(n):
-	if n === 3:
-		a=2
-	else:
-		a=getRandomIntInclusive(2,2*Math.round((Math.log(n))**2))	
-	print(a)
-	if n === 2:
-		return True
+};
+var prime_test = function(n){
+	if (n === 3){
+		a=2;
+	}
+	else{
+		a=getRandomIntInclusive(2,2*Math.round((Math.log(n))**2))
+	}
+	console.log(a)
+	if (n === 2){
+		return true
+	}
 	y=n-1	
-	if (a**(y))%n===1:
-		while y%2===0:
+	if ((a**(y))%n===1){
+		while (y%2===0){
 			y=y/2
 			x=(a**y)%n
-			if x === n-1:
-				return True
-			else: 
-				return False
-	else: 
-		return False
+			if (x === n-1){
+				return true
+			}
+			else{
+				return false
+			}
+		}
+	}
+	else{
+		return false}
+};
